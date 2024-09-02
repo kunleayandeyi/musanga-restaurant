f9dbf9b0ff5aa2b060bd6ce79a312e0e50699fd8
ksquare <kunleayandeyi@gmail.com>
2024-09-02 22:23:22 +0100

    props vs state part 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const App = function () {
+  return (
+    <div className="container">
+      <Header />
+      <Menu/>
+    </div>
+  );
+};
+const Header = function () {
+  return (
+    <header className="header">
+      <h1>Musanga</h1>
+    </header>
+  );
+};
+const Menu=function(){
+  return<main className="menu">
+    <h2> Our Menu speaks</h2>
+<Food/>
+  </main>
+}
+const Food=function(){
+  return<div>
+    <img src='./menu/corn.jpg' alt='corn menu'/>
+    <h3>This menu consist of sweet corn and freshly plucked cucumber and a big green pepper to garnish</h3>
+  </div>
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
